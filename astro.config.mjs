65c5f7639c4d2a92406d03724f9ded67c5bffeab
christian barreto <christianbarreto19@fpuna.edu.py>
2026-02-09 14:35:54 -0300

    Primer commit: agregando archivos del proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "url";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+    service: { entrypoint: "astro/assets/services/noop" },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+        "~": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  },
+});
